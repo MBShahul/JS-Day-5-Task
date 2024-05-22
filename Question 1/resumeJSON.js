a3d36862fde3511373bdb83e73a7d929e07bd718
MBShahul <156427310+MBShahul@users.noreply.github.com>
2024-05-22 22:17:37 +0530

    Update resumeJSON.js

--- Question 1/resumeJSON.js
+++ Question 1/resumeJSON.js
@@ -3,32 +3,15 @@ let myResume = {
     lastName : "Hameed",
     email : "iamshahulms6@gmail.com",
     phone : "9876543210",
-  
-    workExperience : [
-      {
-        company : "Samsung",
-        position : "Software Engineer",
-      },
-    ],
-  
-    education : [
-      {
-        institution : "Global Institute of Technology",
-        degree : "Computer Science Engineering",
-        graduationDate : "2019-05"
-      },
-    ],
-  
-    skills : [
-        "HTML",
-        "CSS",
-        "JavaScript"
-    ],
-  
-    links : {
-        github : "https://github.com",
-        linkedin : "https://www.linkedin.com"
-    }
+    workExperience : "3 Years",
+    company : "Samsung",
+    position : "Software Engineer",
+    education : "Global Institute of Technology",
+    degree : "Computer Science Engineering",
+    graduationDate : "2019-05",
+    skills : "JavaScript",
+    github : "https://github.com",
+    linkedin : "https://www.linkedin.com"
 }
-  
+
 console.log(myResume);
